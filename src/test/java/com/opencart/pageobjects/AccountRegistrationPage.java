74550a2d172a4e5a451f06e60fdc1ea8e72e6e90
Mahoda Adikari <mahodarc97@gmail.com>
2025-01-22 01:19:48 +0530

    Added First Test Case TC001.

--- src/test/java/com/opencart/pageobjects/AccountRegistrationPage.java
+++ src/test/java/com/opencart/pageobjects/AccountRegistrationPage.java
@@ -30,10 +30,52 @@ public class AccountRegistrationPage extends BasePage {
     WebElement txtConfirmPassword;
 
     @FindBy(xpath = "//input[@name='agree']")
-    WebElement chkBoxPrivacyPolicy;
+    WebElement chkdPrivacyPolicy;
 
     @FindBy(xpath = "//input[@value='Continue']")
     WebElement btnContinue;
 
+    @FindBy(xpath = "//h1[normalize-space()='Your Account Has Been Created!']")
+    WebElement msgConfirmation;
 
+
+    public void setTxtFirstName(String firstName) {
+        typeText(this.txtFirstName, firstName);
+    }
+
+    public void setTxtLastName(String lastName) {
+        typeText(this.txtLastName, lastName);
+    }
+
+    public void setTxtEmail(String email) {
+        typeText(this.txtEmail, email);
+    }
+
+    public void setTxtTelephone(String telephone) {
+        typeText(this.txtTelephone, telephone);
+    }
+
+    public void setTxtPassword(String password) {
+        typeText(this.txtPassword, password);
+    }
+
+    public void setTxtConfirmPassword(String confirmPassword) {
+        typeText(this.txtConfirmPassword, confirmPassword);
+    }
+
+    public void setChkdPrivacyPolicy() {
+        clickElement(chkdPrivacyPolicy);
+    }
+
+    public void setBtnContinue() {
+        clickElement(btnContinue);
+    }
+
+    public String getMsgConfirmation() {
+        try {
+            return msgConfirmation.getText();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
 }
